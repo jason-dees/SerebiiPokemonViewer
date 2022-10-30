a673b22f0f7b3f0d1543f1b79e4ded0c91a3b12c
Jason Dees <jhdees@gmail.com>
2022-10-30 13:30:00 -0400

    Initial Commit

---
+++ SerebiiPokemonViewer/SerebiiPokemonViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerebiiPokemonViewerApp.swift
+//  SerebiiPokemonViewer
+//
+//  Created by Jason Dees on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SerebiiPokemonViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
